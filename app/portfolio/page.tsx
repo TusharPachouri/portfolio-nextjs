d1448efb193aaabf4fe4ba0f0c6b0d9961a9a793
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-11-28 01:32:38 +0530

    refactor: Update import paths and file structure for consistency

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,22 @@
+
+import { NextPage } from "next";
+import { 
+  FaUser, 
+  FaBirthdayCake, 
+  FaMapMarkerAlt, 
+  FaLanguage, 
+  FaPhone, 
+  FaEnvelope, 
+  FaBriefcase, 
+  FaHeart, 
+  FaMedal, 
+  FaCloudDownloadAlt 
+} from "react-icons/fa";
+
+const Portfolio: NextPage = () => {
+  return (
+    <h1>Portfolio</h1>
+  );
+};
+
+export default Portfolio;
