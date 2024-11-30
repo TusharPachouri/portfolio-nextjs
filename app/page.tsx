47b8a079f70e42db19a59e8eb2fceea140c73dbb
Tushar Pachouri <tusharpachouri001@gmail.com>
2024-12-01 00:37:12 +0530

    feat: Add Chatwoot widget component for enhanced user interaction

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,14 @@ import {
   FaBlog,
   FaEnvelope,
 } from "react-icons/fa";
+import ChatwootWidget from "@/components/ui/ChatwootWidget";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         {/* <PortfolioLoader> */}
+        <ChatwootWidget />
         <Hero />
         {/* </PortfolioLoader> */}
       </div>
---
+++ components/ui/ChatwootWidget.tsx
@@ -0,0 +1,35 @@
+'use client'
+import React, { useEffect } from 'react';
+
+const ChatwootWidget: React.FC = () => {
+  useEffect(() => {
+    // Add Chatwoot Settings
+    (window as any).chatwootSettings = {
+      hideMessageBubble: false,
+      position: 'right', // This can be left or right
+      locale: 'en', // Language to be set
+      type: 'standard', // [standard, expanded_bubble]
+    };
+
+    // Paste the script from inbox settings except the <script> tag
+    (function (d: Document, t: string) {
+      const BASE_URL = "https://app.chatwoot.com";
+      const g = d.createElement(t) as HTMLScriptElement;
+      const s = d.getElementsByTagName(t)[0];
+      g.src = `${BASE_URL}/packs/js/sdk.js`;
+      g.defer = true;
+      g.async = true;
+      s.parentNode?.insertBefore(g, s);
+      g.onload = () => {
+        (window as any).chatwootSDK.run({
+          websiteToken: process.env.CHATWOOTWIDGET_TOKEN ,
+          baseUrl: BASE_URL,
+        });
+      };
+    })(document, "script");
+  }, []);
+
+  return null;
+};
+
+export default ChatwootWidget;
